da7259c2573ee238ef6ffa4eca97cdbb5550c354
cywen <18205055638@163.com>
2021-03-12 17:20:15 +0800

    配置 quartz

---
+++ wen-quartz/src/main/java/top/flywen/wen/quartz/QuartzApplication.java
@@ -0,0 +1,12 @@
+package top.flywen.wen.quartz;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(QuartzApplication.class, args);
+  }
+}
